c9bc2f35c758a80682c2a0acdd34d7da88b5d083
‘Diontre <diontre01@gmail.com>
2023-04-24 20:27:41 -0500

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: red;
+`
+
+function App() {
+
+  return (
+    <>
+      
+
+      <Container>
+      <h1> Hello </h1>
+      </Container>
+     
+
+    </>
+  )
+}
+
+export default App
